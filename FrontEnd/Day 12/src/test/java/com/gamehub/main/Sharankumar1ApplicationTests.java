b2b1886f34ba0eb7abb5a40dc04cf43dacc997dd
SharankumarD10 <110532026+SharankumarD10@users.noreply.github.com>
2023-10-14 10:48:12 +0530

    Add files via upload

---
+++ FrontEnd/Day 12/src/test/java/com/gamehub/main/Sharankumar1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamehub.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sharankumar1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
